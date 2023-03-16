df2f8ff3501189e405a1a93fa779739203de9cfd
Tiger <tagir.tsarkov@gmail.com>
2023-03-16 13:06:13 +0200

    rr

--- src/components/PhoneImage.js
+++ src/components/PhoneImage.js
@@ -2,14 +2,15 @@ import React from "react";
 import './PhoneImage.css'
 
 export default function(props){
+    const phonepath = process.env.PUBLIC_URL + '/phone20.9.png';
     return(
         <div className="phone-image-container">
             <div className="phone-container position1">
-                <img className="phone-background" src=".././phone20.9.png"/>
+                <img className="phone-background" src={phonepath}/>
                 <img className="inner-img" src={props.path0}/>
             </div>
             <div className="phone-container position2">
-                <img className="phone-background" src=".././phone20.9.png"/>
+                <img className="phone-background" src={phonepath}/>
                 <img className="inner-img" src={props.path1}/>
             </div>
             
