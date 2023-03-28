94b7fd3034d3beb4c272ba8884546f482100b067
Tiger <tagir.tsarkov@gmail.com>
2023-03-28 15:35:06 +0300

    ööö

--- src/header/Skills.js
+++ src/header/Skills.js
@@ -19,7 +19,7 @@ export default function(){
                 <h1 className="skills-title">{fi.skills1}</h1>
                 <div className="skills-container">
                 <Skill img={path + '/skills/logo-css.png'} title="CSS" list={["Selectors","Box model","Transitions","Layout","Typography","Flexbox"]}/>
-                <Skill img={path + '/skills/logo-javascript.png'} title="JavaScript" list={["Functions","Classes","Objects","Lambdas","Destructuring","DOM","Arrays"]}/>
+                <Skill img={path + '/skills/logo-javascript.png'} title="JavaScript" list={["Functions","Classes","Objects","Lambdas","Destructuring","DOM","Arrays","JQuery"]}/>
                 <Skill img={path + '/skills/logo-java.png'} title="Java" list={["Polymorphism","Exceptions","Threads","Lambdas","Access Control","Data Structures","Reflection","Maven","Gradle","Jetty"]}/>
                 <Skill img={path + '/skills/logo-kotlin.png'} title="Kotlin" list={["Data classes","Coroutines","Extension Functions"]}/>
                 <Skill img={path + '/skills/logoCsharp.png'} title="C#" list={["Methods","Classes","Async","Delegates","LINQ"]}/>
@@ -30,7 +30,7 @@ export default function(){
             <h1 className="skills-title">{fi.skills2}</h1>
                 <div className="skills-container">
                 <Skill img={path + '/skills/logo-android.png'} title="Android" list={[,"Activities","Fragments","RecyclerView","Canvas","Layouts and Views","CustomViews","Animations","Testing","Permissions","Data storage","Retrofit"]}/>
-                <Skill img={path + '/skills/logo-react.png'} title="React" list={["Components","State","Props","JSX"]}/>
+                <Skill img={path + '/skills/logo-react.png'} title="React" list={["Components","State","Props","JSX","Hooks"]}/>
                 <Skill img={path + '/skills/logo-unity.png'} title="Unity" list={["Objects","Mesh rendering","Particle Systems","Raycasting","Physics Simulation"]}/>
                 <Skill img={path + '/skills/logo-wicket.png'} title="Apache Wicket" list={["Components","Models","AjaxButton","Dynamic JavaScript"]}/>
                 <Skill img={path + '/skills/logo-springboot.png'} title="Spring Boot" list={["annotations","(in process)"]} />
