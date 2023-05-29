6b3adedaaf671d621b0e363f692374600a4d99c7
Tiger <tagir.tsarkov@gmail.com>
2023-05-29 23:38:51 +0300

    tt

--- src/App.js
+++ src/App.js
@@ -16,27 +16,26 @@ function App() {
     <>
     <div className='wrapper'>
     <ToolBar />
-      <img className='rocks' src={iFolder+'/cliff0.png'} alt=""/>
-      <img className='background-image' src={iFolder+'/cliff1.jpg'} alt=""/>
       
+    <img className='rocks' src={iFolder+'/cliff0.png'} alt=""/>
+      <img className='background-image' src={iFolder+'/cliff1.jpg'} alt=""/>
       <div class="page active">
           <TextBlock title={fi.aboutTitle} text={fi.aboutText} theme="main-title"/>
-          <TextBlock title={fi.actualAboutTitle} text={fi.actualAboutText} theme="about-me"/>
           <Skills className="skills"/>  
       </div>
-
-      <div className='masking-background'></div>
+      <div className='masking-background'></div> 
+      <TextBlock title={fi.actualAboutTitle} text={fi.actualAboutText} theme="about-me"/>
       <div className='masking-background is-white'></div>
 
       <div class="page animation1 ">
-            
+         
 
             <div className="content">
               <div className="phone-image-container ">
                 <PhoneImage width={phoneWidth} height={phoneHeight} video={iFolder + '/topic2/rhythm0000.mp4'} position="position1"/>
                 <PhoneImage width={phoneWidth} height={phoneHeight} path={iFolder + '/topic2/picmetr5.jpg'} position="position2"/>
               </div>
-              <TextBlock theme="topic" title={fi.topic2Title} text={fi.topic2Text} link={"https://github.com/tagir215/VisualMetronome"}/>
+              <TextBlock theme="topic" title={fi.topic2Title} text={fi.topic2Text} list={fi.topic2List} link={"https://github.com/tagir215/VisualMetronome"}/>
             </div>  
  
             <div className="content">
@@ -44,30 +43,36 @@ function App() {
                 <PhoneImage width={phoneWidth} height={phoneHeight} path={iFolder + '/topic3/mindmap1.jpg'} position="position1"/>
                 <PhoneImage width={phoneWidth} height={phoneHeight} video={iFolder + '/topic3/mindmap0000.mp4'} position="position2"/>
               </div>
-              <TextBlock theme="topic" title={fi.topic3Title} text={fi.topic3Text} link={"https://github.com/tagir215/YourMindMap"}/>
+              <TextBlock theme="topic" title={fi.topic3Title} text={fi.topic3Text} list={fi.topic3List} link={"https://github.com/tagir215/YourMindMap"}/>
               
             </div>
 
-       
-          
-          <div className="content">
+            <div className="content">
+              <div className="images-container">
+                  <img className="app-image" src={iFolder+"/topic5/csamma1.png"}></img>
+                  <img className="app-image app-pos1" src={iFolder+"/topic5/csamma2.png"}></img>
+              </div>
+              <TextBlock theme="topic" title={fi.topic5Title} text={fi.topic5Text} list={fi.topic5List} link={"https://github.com/tagir215/CosineSearchAutomaticMindMapApplicationClient"} link2={"https://github.com/tagir215/CosineSearchAutomaticMindMapApplicationServer"}/>
+            </div>
+
+            <div className="content">
               <div className="phone-image-container ">
                 <PhoneImage width={phoneWidth} height={phoneHeight} video={iFolder + '/topic1/starchat0000.mp4'} position="position1"/>
                 <PhoneImage width={phoneWidth} height={phoneHeight} path={iFolder + '/topic1/starchat1.jpg'} position="position2"/>
               </div>
-              <TextBlock theme="topic" title={fi.topic1Title} text={fi.topic1Text} link={"https://github.com/tagir215/Star-Chat"}/>
+              <TextBlock theme="topic" title={fi.topic1Title} text={fi.topic1Text} list={fi.topic1List} link={"https://github.com/tagir215/Star-Chat"}/>
               
-            </div>
+          </div>
 
             <div className="content">
             <div className="phone-image-container ">
                 <PhoneImage width={phoneWidth} height={phoneHeight} path={iFolder + '/topic4/newgolf2.png'} position="position1"/>
                 <PhoneImage width={phoneWidth} height={phoneHeight} video={iFolder + '/topic4/golf0000.mp4'} position="position2"/>
               </div>
-              <TextBlock theme="topic" title={fi.topic4Title} text={fi.topic4Text} link={"https://github.com/tagir215/GPSGolf"}/>
+              <TextBlock theme="topic" title={fi.topic4Title} text={fi.topic4Text} list={fi.topic4List} link={"https://github.com/tagir215/GPSGolf"}/>
             </div>
 
-            
+            <Videos/>
 
             <Bottombar /> 
         </div>
--- src/components/TextBlock.js
+++ src/components/TextBlock.js
@@ -3,14 +3,23 @@ import './TextBlock.css';
 import en from '.././locales/en.json'
 import fi from '.././locales/fi.json'
 
-export default function({title,text, link, theme}){
+export default function({title,text,list, link,link2, theme}){
     
     return(
         <div className={"topic-content "+theme}>
             <h1 className={theme+"-title"} >{title}</h1>
+            
             <span className={theme+"-text"} >{text}</span>
+            {list !=null && <h4>{fi.topicPlus}</h4>}
+            {list != null &&  
+                <ul className="topic-list">
+                {list.map((string,index)=>{
+                    return <li key={index}>{string}</li>
+                })}
+                </ul>}
             <br/>
             {link && <a className="link" target="_blank" href={link}>{fi["learn more"]}</a>}
+            {link2 && <a className="link" target="_blank" href={link2}>{fi["learn more"]}</a>}
         </div>
     )
 }
