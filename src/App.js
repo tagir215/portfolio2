22a201dd32fd83322174d04f77a1c2d090f932e1
Tiger <tagir.tsarkov@gmail.com>
2023-11-03 10:55:35 +0200

    fixing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import TextBlock from './components/TextBlock.js'
 import Bottombar from './header/Bottombar';
 import PhoneImage from './components/PhoneImage';
 import Skills from './header/Skills';
-import Videos from './components/Videos';
 import React, { useState, useEffect } from 'react';
 
 function Page({lang}) {
@@ -14,15 +13,13 @@ function Page({lang}) {
 
   useEffect(() => {
     const backgroundImage = new Image();
-    backgroundImage.src = iFolder + '/cliff1.jpg';
+    backgroundImage.src = iFolder + '/cliff0L.png';
 
     backgroundImage.onload = () => {
       setBackgroundImageLoaded(true);
     };
   }, [iFolder]);
 
-  const phoneWidth = "300px";
-  const phoneHeight = "500px"
   return (
     <>
       {!backgroundImageLoaded && 
@@ -32,8 +29,8 @@ function Page({lang}) {
         }
         <div className={`wrapper ${backgroundImageLoaded ? "visible" : ""}`}>
         <ToolBar lang={lang}/>
-    <img className='rocks' src={iFolder+'/cliff0.png'} alt=""/>
-      <img className='background-image' src={iFolder+'/cliff1.jpg'} alt=""/>
+    <img className='rocks' src={iFolder+'/cliff0L.png'} alt=""/>
+      <img className='background-image' src={iFolder+'/cliff1L.png'} alt=""/>
       <div class="page active">
           <TextBlock lang={lang} title={lang.aboutTitle} text={lang.aboutText} theme="main-title"/>
           <Skills className="skills"/>  
@@ -46,8 +43,8 @@ function Page({lang}) {
 
             <div className="content">
               <div className="images-container">
-                  <img className="app-image" src={iFolder+"/topic6/arp.png"}></img>
-                  <img className="app-image app-pos1" src={iFolder+"/topic6/arp2.png"}></img>
+                  <img className="app-image" src={iFolder+"/topic6/arp.png"} alt=''></img>
+                  <img className="app-image app-pos1" src={iFolder+"/topic6/arp2.png"} alt=''></img>
               </div>
               <TextBlock lang={lang} theme="topic" title={lang.topic6Title} text={lang.topic6Text} list={lang.topic6List} link={"https://github.com/tagir215/AfricanRockPythonServer"} link2={"https://github.com/tagir215/african-rock-python-client"} link3={"https://arp-client-c1df0d4c8a26.herokuapp.com"}/>
             </div>
@@ -55,8 +52,8 @@ function Page({lang}) {
 
             <div className="content">
               <div className="phone-image-container ">
-                <PhoneImage width={phoneWidth} height={phoneHeight} video={iFolder + '/topic1/starchat0000.mp4'} position="position1"/>
-                <PhoneImage width={phoneWidth} height={phoneHeight} path={iFolder + '/topic1/starchat1.jpg'} position="position2"/>
+                <PhoneImage video={iFolder + '/topic1/starchat0000.mp4'} position="position1"/>
+                <PhoneImage path={iFolder + '/topic1/starchat1.jpg'} position="position2"/>
               </div>
               <TextBlock lang={lang} theme="topic" title={lang.topic1Title} text={lang.topic1Text} list={lang.topic1List} link={"https://github.com/tagir215/Star-Chat"}/>
               
@@ -66,24 +63,24 @@ function Page({lang}) {
 
             <div className="content">
               <div className="images-container">
-                  <img className="app-image" src={iFolder+"/topic5/csamma1.png"}></img>
-                  <img className="app-image app-pos1" src={iFolder+"/topic5/csamma2.png"}></img>
+                  <img className="app-image" src={iFolder+"/topic5/csamma1.png"} alt=''></img>
+                  <img className="app-image app-pos1" src={iFolder+"/topic5/csamma2.png"} alt=''></img>
               </div>
               <TextBlock lang={lang} theme="topic" title={lang.topic5Title} text={lang.topic5Text} list={lang.topic5List} link2={"https://github.com/tagir215/CosineSearchAutomaticMindMapApplicationClient"} link={"https://github.com/tagir215/CosineSearchAutomaticMindMapApplicationServer"}/>
             </div>
 
             <div className="content">
               <div className="phone-image-container ">
-                <PhoneImage width={phoneWidth} height={phoneHeight} video={iFolder + '/topic2/rhythm0000.mp4'} position="position1"/>
-                <PhoneImage width={phoneWidth} height={phoneHeight} path={iFolder + '/topic2/picmetr5.jpg'} position="position2"/>
+                <PhoneImage video={iFolder + '/topic2/rhythm0000.mp4'} position="position1"/>
+                <PhoneImage path={iFolder + '/topic2/picmetr5.jpg'} position="position2"/>
               </div>
               <TextBlock lang={lang} theme="topic" title={lang.topic2Title} text={lang.topic2Text} list={lang.topic2List} link={"https://github.com/tagir215/VisualMetronome"}/>
             </div>  
  
             <div className="content">
               <div className="phone-image-container ">
-                <PhoneImage width={phoneWidth} height={phoneHeight} path={iFolder + '/topic3/mindmap1.jpg'} position="position1"/>
-                <PhoneImage width={phoneWidth} height={phoneHeight} video={iFolder + '/topic3/mindmap0000.mp4'} position="position2"/>
+                <PhoneImage path={iFolder + '/topic3/mindmap1.jpg'} position="position1"/>
+                <PhoneImage video={iFolder + '/topic3/mindmap0000.mp4'} position="position2"/>
               </div>
               <TextBlock lang={lang} theme="topic" title={lang.topic3Title} text={lang.topic3Text} list={lang.topic3List} link={"https://github.com/tagir215/YourMindMap"}/>
               
@@ -93,8 +90,8 @@ function Page({lang}) {
 
             <div className="content">
             <div className="phone-image-container ">
-                <PhoneImage width={phoneWidth} height={phoneHeight} path={iFolder + '/topic4/newgolf2.png'} position="position1"/>
-                <PhoneImage width={phoneWidth} height={phoneHeight} video={iFolder + '/topic4/golf0000.mp4'} position="position2"/>
+                <PhoneImage path={iFolder + '/topic4/newgolf2.png'} position="position1"/>
+                <PhoneImage video={iFolder + '/topic4/golf0000.mp4'} position="position2"/>
               </div>
               <TextBlock lang={lang} theme="topic" title={lang.topic4Title} text={lang.topic4Text} list={lang.topic4List} link={"https://github.com/tagir215/GPSGolf"}/>
             </div>
--- src/components/PhoneImage.js
+++ src/components/PhoneImage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './PhoneImage.css'
 
-export default function(props){
+export default function PhoneImage(props){
     const phonepath = process.env.PUBLIC_URL + '/phone20.9.png';
     return(
-        <div className={"phone-container "+props.position} style={{width:`${props.width}`,height:`${props.height}`}}>
-            {!props.img && <img className="phone-background" loading="lazy" src={phonepath}/>}
-            {props.img && <img className="phone-background" loading="lazy" src={props.img}/>}
-            {props.path && <img className="inner-img" loading="lazy" src={props.path}/>} 
+        <div className={"phone-container "+props.position} >
+            {!props.img && <img className="phone-background" alt="" loading="lazy" src={phonepath}/>}
+            {props.img && <img className="phone-background" alt="" loading="lazy" src={props.img}/>}
+            {props.path && <img className="inner-img" alt="" loading="lazy" src={props.path}/>} 
             {props.video && <video className="inner-img" loading="lazy" src={props.video} autoPlay muted loop />}
         </div>
     )
